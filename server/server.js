68564bed4d2e8d8d6f309306c343aa0223a42f4a
rush <rushastronomo0415@gmail.com>
2019-07-07 00:41:53 -0700

    setup containers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
+const axios = require('axios');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
+
+app.get('/movies/:title', (req, res) => {
+  movieTitle = req.params.title;
+  return axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${movieTitle}`)
+    .then( response => res.send(response.data))
+    .catch( err => console.log(err))
+});
 
 module.exports = app;
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+// import reducers from './reducers';
 import App from './app';
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// const rootReducer = combineReducers({
+//   movieData: searchReducer
+// });
 
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(
+    promiseMiddleware()
+  )
+));
+
+/* Update render method invoke */
 render(
   <Provider store={ store }>
     <App />
