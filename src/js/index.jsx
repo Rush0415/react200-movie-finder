13e0731092e4d6eecf6d3d86c6eca3b82937cc52
rush <rushastronomo0415@gmail.com>
2019-07-07 00:53:04 -0700

    modefied index.jsx

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,15 +3,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
-// import reducers from './reducers';
+
 import App from './app';
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const rootReducer = combineReducers({
-//   movieData: searchReducer
-// });
 
 const store = createStore(reducers, composeEnhancers(
   applyMiddleware(
