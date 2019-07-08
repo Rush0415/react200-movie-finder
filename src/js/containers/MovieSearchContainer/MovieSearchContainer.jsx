799862db4515b4860f95254a6493c793ae3d23ae
rush <rushastronomo0415@gmail.com>
2019-07-08 12:07:04 -0700

    modified package.json and package-lock.json

--- src/js/containers/MovieSearchContainer/MovieSearchContainer.jsx
+++ src/js/containers/MovieSearchContainer/MovieSearchContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
 class MovieSearchContainer extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const search = this.props;
     return (
       <div>
         <h1>Movie Search Container</h1>
